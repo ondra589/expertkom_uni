8c1d1fe04aaf5ea29892b80118e1c7337d1aa77d
Ondra <Ondra@192.168.0.73>
2018-12-18 17:37:54 +0100

    donahrání

--- src/lekce_2/ukol_1.java
+++ src/lekce_2/ukol_1.java
@@ -7,7 +7,7 @@ public class ukol_1 {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Stiskni tlaèítko a enter: ");
 		char keyboardEntry = (char) System.in.read();
-		System.out.println("Vložil jsi: "+keyboardEntry);
+		System.out.println("Zadal jsi: "+keyboardEntry);
 
 	}
 
